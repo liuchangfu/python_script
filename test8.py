5921c1feccc0bab6cff4d8a92f0518d8ed6e6702
liuchangfu <shift_1220@163.com>
2017-04-21 22:39:27 +0800

    python练习脚本

---
+++ test8.py
@@ -0,0 +1,10 @@
+guess=int(input('你的数是字是多少：'))
+ser=2
+if guess == ser:
+    print('哎呀，你竟然猜中了!!!')
+    print('然而并没有什么卵用！')
+else:
+    if guess == ser:
+        print('多了！！！')
+    else:
+        print('少了！！！')
