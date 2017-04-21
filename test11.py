19b8ad5175cdfa66b9e8104214d184905d7bab20
liuchangfu <shift_1220@163.com>
2017-04-21 22:44:59 +0800

    if语句中的range（）使用方法

---
+++ test11.py
@@ -0,0 +1,9 @@
+print('传递两个参数的range()')
+for i in range(5):
+    print(i)
+print('传递两个参数的range()')
+for i in range(2,9):
+    print(i)
+print('传递三个参数的range()')
+for i in range(1,10,2):
+    print(i)
