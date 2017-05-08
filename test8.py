22bc78725b36119d854aa69a020ed2993c0d99a7
liuchangfu <shift_1220@163.com>
2017-05-08 14:57:38 +0800

    优化猜数字游戏，猜对3次退出

--- test8.py
+++ test8.py
@@ -1,10 +1,17 @@
-guess=int(input('你的数是字是多少：'))
-ser=2
-if guess == ser:
-    print('哎呀，你竟然猜中了!!!')
-    print('然而并没有什么卵用！')
-else:
-    if guess == ser:
-        print('多了！！！')
+print("你只有三次输入机会哦!!")
+temp=input('你的数是字是多少：')
+guess = int(temp)
+i=1
+while guess != 8 and i < 3:
+    temp = input("猜错了，请重新输入吧：")
+    i = i+1
+    guess = int(temp)
+    if guess == 8:
+        print('哎呀，你竟然猜中了!!!')
+        print('然而并没有什么卵用！')
     else:
-        print('少了！！！')
+        if guess > 8:
+            print('大了！！！')
+        else:
+            print('少了！！！')
+print("游戏结束，不玩了！！！")
