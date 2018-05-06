6e650027218c1518f53d2c1becd45a650c877c30
liuchangfu <shift_1220@163.com>
2018-05-06 17:07:05 +0800

    提交django例子

---
+++ mysite/blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import BlogArticles
+
+# Register your models here.
+class BlogArticclesAdmin(admin.ModelAdmin):
+    list_display = ('id','title','author','publish')
+    list_filter = ('publish','author')
+    search_fields = ('title','body')
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['publish','author']
+
+admin.site.register(BlogArticles,BlogArticclesAdmin)
---
+++ mysite/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class BlogArticles(models.Model):
+    # title属性为CharFiel()类型，并且以参数max_length的形式说明字段的最大的数量
+    title = models.CharField(max_length=300)
+
+    author = models.ForeignKey(User,related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
